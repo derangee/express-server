a54ab7a649e4748594f93bc6fec0fa78563f32a1
derangee <daksh_vashishtha@srmap.edu.in>
2025-02-01 01:48:03 +0530

    Update app.js

--- express/app.js
+++ express/app.js
@@ -3,17 +3,19 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => { //First Middleware
-    console.log("First middleware called");
+app.use("/", (req, res, next) => { //First Middleware
+    console.log("First middleware called" ,req.url, req.path);
     next(); //Function to trigger the next middleware
 })
 
-app.use((req, res, next) => { //Second Middleware
-    console.log("Second middleware called");
+app.use("/contact", (req, res, next) => { //Second Middleware
+    console.log("Second middleware called" , req.url, req.path);
+
 })
 
 
-const PORT = 5050;
+
+const PORT = 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server running at http://localhost:${PORT}`);
