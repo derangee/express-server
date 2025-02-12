b471f3a7d7a8de2be7cf777640365e7ca89cfe7b
derangee <daksh_vashishtha@srmap.edu.in>
2025-02-12 18:11:59 +0530

    views - host & store

--- Airbnb Backend/controllers/home.js
+++ Airbnb Backend/controllers/home.js
@@ -6,17 +6,17 @@ exports.setHomes = (homesArray) => {
 };
 
 exports.addHome = (req, res, next) => {
-    res.render('add-home')
+    res.render('host/add-home')
 };
 
 exports.homeAdded = (req, res, next) => {
     const newHome = new HomeModel(req.body.home, req.body.location, req.body.price, req.body.photo)
     newHome.save(); 
-    res.render('home-added')
+    res.render('host/home-added')
 };
 
 exports.homePage = (req, res) => {
     const homes = HomeModel.fetchAll(homes => {
-        res.render('home', { homes: homes })
+        res.render('store/home', { homes: homes })
     }); 
 }
